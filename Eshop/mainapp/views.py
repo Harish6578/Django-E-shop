b102ad20e23977b6a098d9468a6e68526b339110
Harish6578 <harishvarma6578@gmail.com>
2026-01-05 11:18:31 +0530

    Upadate files

--- Eshop/mainapp/views.py
+++ Eshop/mainapp/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def homeView(request):
+    template ='mainapp/home.html'
+    context = {}
+
+    return render(request, template_name=template, context= context)
+
+def aboutView(request):
+    template = 'mainapp/about.html'
+    context={}
+
+    return render(request, template_name=template,context= context)
+
+def contectView(request):
+    template = 'mainapp/contect.html'
+    context={}
+
+    return render(request, template_name=template,context= context)
+
