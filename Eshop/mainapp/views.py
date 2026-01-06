48b3f88457234545f93fe2d2e94c020166e0d9ac
Harish6578 <harishvarma6578@gmail.com>
2026-01-06 11:17:44 +0530

    Upadate files

--- Eshop/mainapp/views.py
+++ Eshop/mainapp/views.py
@@ -1,22 +1,33 @@
 from django.shortcuts import render
 
+from .models import CarouselImage
+
 # Create your views here.
 
 def homeView(request):
     template ='mainapp/home.html'
-    context = {}
+    context = {
+        'current_page': 'Home',
+
+        #Let's collect all existing record of carousel image table to be sent to template
+        'carousel_images' : CarouselImage.objects.all() # select * from carousel_image;
+    }
 
     return render(request, template_name=template, context= context)
 
 def aboutView(request):
     template = 'mainapp/about.html'
-    context={}
+    context={
+          'current_page' : 'About'
+    }
 
     return render(request, template_name=template,context= context)
 
 def contactView(request):
     template = 'mainapp/contact.html'
-    context={}
+    context={
+        'current_page' : 'Contact'
+    }
 
     return render(request, template_name=template,context= context)
 
