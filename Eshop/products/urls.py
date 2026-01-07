c47ef5ec5ef8ef7deeb06c98edee1d4f7d6a8784
Harish6578 <harishvarma6578@gmail.com>
2026-01-07 10:44:17 +0530

    Upadate files

---
+++ Eshop/products/urls.py
@@ -0,0 +1,16 @@
+# from django.urls import path
+
+# from . import views
+
+# urlpatterns = [
+#     path('',views.homeView, name ='home_page'),
+#     path("about/",views.aboutView, name = 'about_page'),
+#     path("contact/",views.contactView, name = 'contact_page')
+# ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.productsView)
+]
--- Eshop/products/views.py
+++ Eshop/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
+def productsView(request):
+    template = 'products/products.html'
+    context ={
+        'products' : 'Product'
+    }
+
+    return render(request,template_name=template,context=context)
