fc4795208f34b52c2a8a4b74a5496b0358fc6c83
Harish6578 <harishvarma6578@gmail.com>
2026-01-08 10:41:19 +0530

    Upadate files

--- Eshop/products/urls.py
+++ Eshop/products/urls.py
@@ -1,16 +1,8 @@
-# from django.urls import path
 
-# from . import views
-
-# urlpatterns = [
-#     path('',views.homeView, name ='home_page'),
-#     path("about/",views.aboutView, name = 'about_page'),
-#     path("contact/",views.contactView, name = 'contact_page')
-# ]
 
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.productsView)
+    path('', views.productsView, name='Product')
 ]
