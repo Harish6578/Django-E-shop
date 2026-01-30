aba365b468fe41ceb3ac357ff63f58a3b9911605
Harish6578 <harishvarma6578@gmail.com>
2026-01-30 12:37:43 +0530

    Upadate files

--- Eshop/authentication/views.py
+++ Eshop/authentication/views.py
@@ -9,18 +9,22 @@ from django.contrib.auth.models import User
 from django.contrib.auth.views import(
     LoginView
 )
-
 # CreateView 
 from django.views.generic import CreateView
+
+from .forms import UserRegisterForm,UserLoginForm
 
 # Create your views here.
 
 class UserRegisterView(CreateView):
     model=User
+    form_class=UserRegisterForm
     template_name='authentication/register.html'
-    fields=['username','password']
     success_url =reverse_lazy('signin')
 
 
 class UserLoginView(LoginView):
     template_name='authentication/login.html'
+    authentication_form=UserLoginForm
+
+
