cfd47969b4f677ce50657fff2156b314986f0014
Harish6578 <harishvarma6578@gmail.com>
2026-02-02 12:28:42 +0530

    Updated

--- Eshop/static/js/scripts.js
+++ Eshop/static/js/scripts.js
@@ -6,8 +6,6 @@ const products_container=document.getElementById('products-container');
 
 let count_cart = document.getElementById('count_cart')
 
-function getCookie(name){
-    let cookieValue=null;
-    if(document.cookie && document.cookie !==)
-        const cookies =
-    }
+const csrfToken =document.querySelector("[name=csrfmiddlewaretoken").value
+
+alert(csrfToken)
